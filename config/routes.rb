13d51cf0055853c95fcb5b19c430df937a8cfa6f
TakeruUshio <ta-ushio@groovenauts.jp>
2017-04-05 19:54:41 +0900

    add hello world

--- config/routes.rb
+++ config/routes.rb
@@ -53,4 +53,7 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
+  Rails.application.routes.drow do
+    root 'application#hello'
+  end
 end
